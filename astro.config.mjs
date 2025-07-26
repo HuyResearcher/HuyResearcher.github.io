2db12e5a7c4f1e6415109b99d1bbaecc80c2e35f
HuyResearcher <crizto7012@gmail.com>
2025-07-26 15:36:45 +0700

    page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://HuyResearcher.github.io',
-  base: '/portfolio.dev',
+  site: 'https://quochuyba.gitbub.io',
 })
